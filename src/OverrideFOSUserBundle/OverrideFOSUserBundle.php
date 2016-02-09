a94c2e772b1f7b98ff10ecdd76c384268cdc0449
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-09 00:41:01 +0000

    - Major commit for OverrideFOSUserBundle templates (Login and Base template)

---
+++ src/OverrideFOSUserBundle/OverrideFOSUserBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OverrideFOSUserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OverrideFOSUserBundle extends Bundle
+{
+
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+
+}
