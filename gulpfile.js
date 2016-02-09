2ed1db4281e5540836bb4186360c844ca9bb9fc0
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-09 00:41:46 +0000

    - Added gulp-image that will allow to compress and move images from bundles to web/img

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var uglify = require("gulp-uglify");
 var path = require('path');
 var minifyCSS = require('gulp-minify-css');
 var rename = require("gulp-rename");
+var image = require('gulp-image');
 
 gulp.task('minify-js', function () {
     gulp.src('./src/**/**/Resources/public/js/*.js') // path to your files
@@ -20,7 +21,14 @@ gulp.task('minifylesstocss', function () {
             .pipe(gulp.dest('web/css'));
 });
 
-gulp.task('default', ['minifylesstocss', 'minify-js'], function () {
+gulp.task('image', function () {
+  gulp.src('./src/**/**/Resources/public/img/*')
+    .pipe(image())
+    .pipe(rename({dirname: ''}))
+    .pipe(gulp.dest('web/img'));
+});
+
+gulp.task('default', ['minifylesstocss', 'minify-js', 'image'], function () {
     console.log("Started gulp");
 });
 
@@ -30,4 +38,8 @@ gulp.watch('./src/**/**/Resources/public/less/*.less', ['minifylesstocss'], func
 
 gulp.watch('./src/**/**/Resources/public/js/*.js', ['minify-js'], function () {
     console.log("Compressing js");
+});
+
+gulp.watch('./src/**/**/Resources/public/img/*', ['image'], function () {
+    console.log("Compressing images");
 });
