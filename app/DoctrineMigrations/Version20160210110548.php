0773f370c7118a486135b8c182a0481486db8b69
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-10 10:41:45 +0000

    - Added Category Migration

---
+++ app/DoctrineMigrations/Version20160210110548.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+use UniquePasswordBundle\Entity\Category;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160210110548 extends AbstractMigration implements ContainerAwareInterface {
+
+    private $container;
+    
+    private $em;
+
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+        $this->em = $this->container->get('doctrine')->getManager('default');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema) {
+        $arryOfCategories = array(
+            array('name' => 'Login', 'icon' => 'fa-sign-in'),
+            array('name' => 'Credit Card', 'icon' => 'fa-credit-card'),
+            array('name' => 'Note', 'icon' => 'fa-sticky-note-o')
+        );
+        foreach ($arryOfCategories as $category) {
+            $newCategory = new Category;
+
+            $newCategory->setName($category['name']);
+            $newCategory->setIcon($category['icon']);
+
+            $this->em->persist($newCategory);
+            $this->em->flush();
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema) {
+        $categories = $this->container->get('doctrine')->getRepository('UniquePasswordBundle:Category')->findAll();
+        foreach ($categories as $category) {
+            $this->em->remove($category);
+            $this->em->flush();
+        }
+    }
+
+}
