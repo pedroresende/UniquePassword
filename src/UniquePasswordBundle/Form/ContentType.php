77047d0d49f964c1e16110a9045900494f5afb62
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-10 09:47:18 +0000

    - Fixed in Unused use statement should be avoided

--- src/UniquePasswordBundle/Form/ContentType.php
+++ src/UniquePasswordBundle/Form/ContentType.php
@@ -6,10 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 
 class ContentType extends AbstractType
 {
