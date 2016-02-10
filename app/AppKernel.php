46fbc2faee65cb8b95426bd611cf56aafdcc3d20
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-10 10:04:53 +0000

    - Added DoctrineMigrationsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new OverrideFOSUserBundle\OverrideFOSUserBundle(),
             new OverrideAdminThemeBundle\OverrideAdminThemeBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
