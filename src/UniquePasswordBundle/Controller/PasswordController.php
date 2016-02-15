5fd18aba5eb706e91a7a624dda05dc46cd0b99b1
Pedro Resende <pedroresende@mail.resende.biz>
2016-02-15 02:05:20 +0000

    - [WIP]: Build controller to store the new content

--- src/UniquePasswordBundle/Controller/PasswordController.php
+++ src/UniquePasswordBundle/Controller/PasswordController.php
@@ -3,6 +3,8 @@
 namespace UniquePasswordBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use UniquePasswordBundle\Entity\Content;
 use UniquePasswordBundle\Form\ContentType;
 
@@ -20,6 +22,14 @@ class PasswordController extends Controller
         $form = $this->createForm(ContentType::class, $content);
 
         return $this->render('UniquePasswordBundle:Password:add.html.twig',['form' => $form->createView()]);
+    }
+    
+    public function addNewAction(Request $request)
+    {
+        var_dump(json_decode($request->getContent())->senddata);
+        
+        $response = new Response();
+        return $response;
     }
 
     public function listAction()
