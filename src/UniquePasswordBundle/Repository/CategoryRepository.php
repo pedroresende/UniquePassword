59f53def6b286b17d5ec1c405f075fb7e13eea41
Pedro Resende <pedroresende@mail.resende.biz>
2016-03-06 22:19:40 +0000

    - Refactored findNumberOfPasswords

--- src/UniquePasswordBundle/Repository/CategoryRepository.php
+++ src/UniquePasswordBundle/Repository/CategoryRepository.php
@@ -11,14 +11,21 @@ namespace UniquePasswordBundle\Repository;
 class CategoryRepository extends \Doctrine\ORM\EntityRepository
 {
 
+    /**
+     * This method is responsible for fetching the number of stored elements of 
+     * a given category name
+     * 
+     * @param string $name Name of the Category to search
+     * @return int Number of results
+     */
     public function findNumberOfPasswords($name)
     {
         return $this->getEntityManager()
-                        ->createQuery(
-                                'SELECT count(p) FROM UniquePasswordBundle:Content p, UniquePasswordBundle:Category c WHERE p.category = :name'
-                        )
-                        ->setParameter('name', $name)
-                        ->getResult();
+            ->createQuery(
+                    'SELECT count(p) FROM UniquePasswordBundle:Content p, UniquePasswordBundle:Category c WHERE c.id = p.category AND c.name = :name'
+            )
+            ->setParameter('name', $name)
+            ->getResult();
     }
 
 }
