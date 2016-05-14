7d6166ec2876d5582db7f4dba2a3a36022c239f4
Pedro Resende <pedroresende@mail.resende.biz>
2016-05-14 23:07:34 +0100

    - Added updateLogin from the CreateContent Helper Service

--- src/UniquePasswordBundle/Helper/CreateContent.php
+++ src/UniquePasswordBundle/Helper/CreateContent.php
@@ -62,6 +62,42 @@ class CreateContent
     }
 
     /**
+     * This method is responsible updating an existing Entry
+     *
+     * @param stdObject $entryContent contains the content that has been passed
+     * by the form
+     * @param Symfony\Bundle\FrameworkBundle\Controller $user
+     * @return type
+     */
+    public function updateEntry($id, $entryContent, $user)
+    {
+        switch ($entryContent->category) {
+            case '1': {
+                    $this->updateLogin($id, $entryContent, $user);
+                    $this->logger->info('Updated and existing Login entry to the database');
+                    return ['httpStatus' => Response::HTTP_ACCEPTED, 'message' => 'Updated and existing Login information added'];
+                    //break;
+                }
+            case '2': {
+                    $this->updateCreditCard($id, $entryContent, $user);
+                    $this->logger->info('Updated and existing Credit Card entry to the database');
+                    return Response::HTTP_ACCEPTED;
+                    //break;
+                }
+            case '3': {
+                    $this->updateNote($id, $entryContent, $user);
+                    $this->logger->info('Updated and existing  Note entry to the database');
+                    return Response::HTTP_ACCEPTED;
+                    //break;
+                }
+            default: {
+                    return Response::HTTP_BAD_REQUEST;
+                    //break; 
+                }
+        }
+    }
+
+    /**
      * This method is responsible for creating a new Login Class Category
      *
      * @param stdObject $entryContent contains the content that has been passed
@@ -74,6 +110,21 @@ class CreateContent
         $loginContent->setBase($entryContent->siteUsername, $entryContent->sitePasword, $entryContent->siteSitename);
 
         $this->createContent($entryContent, $loginContent, $user);
+    }
+
+    /**
+     * This method is responsible for updating a new Login Class Category
+     *
+     * @param stdObject $entryContent contains the content that has been passed
+     * by the form
+     * @param Symfony\Bundle\FrameworkBundle\Controller $user
+     */
+    private function updateLogin($id, $entryContent, $user)
+    {
+        $loginContent = new Login();
+        $loginContent->setBase($entryContent->siteUsername, $entryContent->sitePasword, $entryContent->siteSitename);
+
+        $this->updateContent($id, $entryContent, $loginContent, $user);
     }
 
     /**
@@ -100,6 +151,33 @@ class CreateContent
         $content->setCategory($this->getCategoryById($entryContent->category));
 
         $content->setCreated($dateNow);
+        $content->setModified($dateNow);
+
+        $this->em->persist($content);
+        $this->em->flush();
+    }
+
+    /**
+     * This method is responsible for updating an existing content
+     * into the database
+     *
+     * @param stdObject $entryContent contains the content that has been passed
+     * by the form
+     * @param UniquePasswordBundle\Category\{Login|CreditCard|Note} $type object to
+     * be constructed
+     * @param Symfony\Bundle\FrameworkBundle\Controller $user
+     */
+    private function updateContent($id, $entryContent, $type, $user)
+    {
+        $dateNow = new \DateTime();
+
+        $content = $this->em->getRepository('UniquePasswordBundle:Content')->find($id);
+
+        $content->setName($entryContent->name);
+        $encodedContent = $type->encode($this->container, $user->getPassword());
+
+        $content->setContent($encodedContent);
+
         $content->setModified($dateNow);
 
         $this->em->persist($content);
